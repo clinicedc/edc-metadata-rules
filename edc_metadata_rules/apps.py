ffa5a2b37ba2d28d8c63555bce75e0d1b090e6db
erikvw <ew2789@gmail.com>
2020-08-24 10:08:25 -0500

    warn if not loading

--- edc_metadata_rules/apps.py
+++ edc_metadata_rules/apps.py
@@ -18,7 +18,11 @@ class AppConfig(DjangoAppConfig):
         sys.stdout.write(f"Loading {self.name} ...\n")
         site_metadata_rules.autodiscover()
         if not site_metadata_rules.registry:
-            sys.stdout.write(style.ERROR(" Warning. No metadata rules have loaded.\n"))
+            sys.stdout.write(
+                style.ERROR(
+                    " Warning. No metadata rules were loaded by autodiscover.\n"
+                )
+            )
         if not self.metadata_rules_enabled:
             sys.stdout.write(style.NOTICE(" * metadata rules disabled!\n"))
         sys.stdout.write(f" Done loading {self.name}.\n")
